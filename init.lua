84b81e333b9bc72084d49d381c5e13487730e354
Zemtzov7 <72821250+zmv7@users.noreply.github.com>
2023-10-27 01:06:49 +0500

    Fix typo

--- init.lua
+++ init.lua
@@ -2,6 +2,6 @@ minetest.register_on_punchnode(function(pos,node)
 	local witem = minetest.localplayer:get_wielded_item()
 	local wear = witem and witem:get_wear()
 	if wear and wear > 60000 then
-		minetest.display_chat_message(minetest.colorize("#ff0","/!\\ High tool hear warning /!\\"))
+		minetest.display_chat_message(minetest.colorize("#ff0","/!\\ High tool wear warning /!\\"))
 	end
 end)
